d96053d5e699f7f776010a7ebabd6665a9e05322
Mark <mfoster9778@gmail.com>
2021-02-18 15:03:38 +0000

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
+
+// Alt set up
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss')('./tailwind.config.js'),
+//     require('autoprefixer')
+//   ]
+// };
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+  },
+
+};
